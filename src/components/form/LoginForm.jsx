0cb67923d5b40d054de762e31ee1066e87b0c9cf
alizada sharifi <alizadasharifi2022@gmail.com>
2025-07-20 16:36:29 +0430

    wrap register form with suspense

--- src/components/form/LoginForm.jsx
+++ src/components/form/LoginForm.jsx
@@ -7,6 +7,7 @@ import useForm, { FormProvider } from "@/hooks/useForm";
 import { loginSchema } from "@/schemas";
 import { Lock, Mail } from "lucide-react";
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default function LoginForm() {
   const { handleSubmit, ...methods } = useForm(loginSchema);
@@ -16,27 +17,27 @@ export default function LoginForm() {
   }
   return (
     <>
-      <FormProvider {...methods}>
-        <form onSubmit={handleSubmit(onSubmit)} className="w-full space-y-4">
-          <Input name="email" placeholder="ایمیل" Icon={Mail} />
-          <Input
-            name="password"
-            placeholder="رمز عبور"
-            Icon={Lock}
-            type="password"
-          />
-          <Button className="w-full">ورود</Button>
-        </form>
-      </FormProvider>
+      <Suspense fallback={"loading..."}>
+        <FormProvider {...methods}>
+          <form onSubmit={handleSubmit(onSubmit)} className="w-full space-y-4">
+            <Input name="email" placeholder="ایمیل" Icon={Mail} />
+            <Input
+              name="password"
+              placeholder="رمز عبور"
+              Icon={Lock}
+              type="password"
+            />
+            <Button className="w-full">ورود</Button>
+          </form>
+        </FormProvider>
 
-      <p>
-        قبلا ثبت نام نکردی؟{" "}
-        <Link className="text-primary" href={"/register"}>
-          ثبت نام
-        </Link>
-      </p>
-
-      
+        <p>
+          قبلا ثبت نام نکردی؟{" "}
+          <Link className="text-primary" href={"/register"}>
+            ثبت نام
+          </Link>
+        </p>
+      </Suspense>
     </>
   );
 }
--- src/components/form/RegisterForm.jsx
+++ src/components/form/RegisterForm.jsx
@@ -8,7 +8,7 @@ import { registerSchema } from "@/schemas";
 import { Lock, Mail, User } from "lucide-react";
 import Link from "next/link";
 import { useQueryState } from "nuqs";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 
 export default function RegisterForm() {
   const [activeSection, setActiveSection] = useQueryState("activeSection");
@@ -27,66 +27,68 @@ export default function RegisterForm() {
 
   return (
     <>
-      <div className="flex w-full bg-neutral-50 py-1 rounded-2xl justify-between px-4 gap-2">
-        <Button
-          className={`w-1/2 font-semibold rounded-2xl ${
-            activeSection === "agency" ? "text-neutral-400" : "text-black"
-          }`}
-          variant={activeSection === "tenant" ? "outline" : "ghost"}
-          onClick={() => setActiveSection("tenant")}
-        >
-          مالک | مستاجر
-        </Button>
-        <Button
-          className={`w-1/2 font-semibold rounded-2xl ${
-            activeSection === "tenant" ? "text-neutral-400" : "text-black"
-          }`}
-          variant={activeSection === "agency" ? "outline" : "ghost"}
-          onClick={() => setActiveSection("agency")}
-        >
-          آژانس مالک
-        </Button>
-      </div>
+      <Suspense fallback={"loading..."}>
+        <div className="flex w-full bg-neutral-50 py-1 rounded-2xl justify-between px-4 gap-2">
+          <Button
+            className={`w-1/2 font-semibold rounded-2xl ${
+              activeSection === "agency" ? "text-neutral-400" : "text-black"
+            }`}
+            variant={activeSection === "tenant" ? "outline" : "ghost"}
+            onClick={() => setActiveSection("tenant")}
+          >
+            مالک | مستاجر
+          </Button>
+          <Button
+            className={`w-1/2 font-semibold rounded-2xl ${
+              activeSection === "tenant" ? "text-neutral-400" : "text-black"
+            }`}
+            variant={activeSection === "agency" ? "outline" : "ghost"}
+            onClick={() => setActiveSection("agency")}
+          >
+            آژانس مالک
+          </Button>
+        </div>
 
-      <FormProvider {...methods}>
-        <form onSubmit={handleSubmit(onSubmit)} className="w-full space-y-4">
-          <div className="flex gap-2">
-            <Input name="name" placeholder="نام" Icon={User} />
-            <Input name="family" placeholder="نام خانوادگی" Icon={User} />
-          </div>
-          <Input name="email" placeholder="ایمیل" Icon={Mail} />
-          <Input
-            name="password"
-            placeholder="رمز عبور"
-            Icon={Lock}
-            type="password"
-          />
-          <div className="flex items-center gap-1">
-            <input
-              type="checkbox"
-              name="rules"
-              id="rules"
-              className="size-4"
-              required
+        <FormProvider {...methods}>
+          <form onSubmit={handleSubmit(onSubmit)} className="w-full space-y-4">
+            <div className="flex gap-2">
+              <Input name="name" placeholder="نام" Icon={User} />
+              <Input name="family" placeholder="نام خانوادگی" Icon={User} />
+            </div>
+            <Input name="email" placeholder="ایمیل" Icon={Mail} />
+            <Input
+              name="password"
+              placeholder="رمز عبور"
+              Icon={Lock}
+              type="password"
             />
-            <label htmlFor="rules" className="text-sm text-neutral-300">
-              با قوانین{" "}
-              <Link href={"/"} className="text-primary font-semibold">
-                رنتی فای
-              </Link>{" "}
-              موافق هستم
-            </label>
-          </div>
-          <Button className="w-full">تایید و دریافت کد</Button>
-        </form>
-      </FormProvider>
+            <div className="flex items-center gap-1">
+              <input
+                type="checkbox"
+                name="rules"
+                id="rules"
+                className="size-4"
+                required
+              />
+              <label htmlFor="rules" className="text-sm text-neutral-300">
+                با قوانین{" "}
+                <Link href={"/"} className="text-primary font-semibold">
+                  رنتی فای
+                </Link>{" "}
+                موافق هستم
+              </label>
+            </div>
+            <Button className="w-full">تایید و دریافت کد</Button>
+          </form>
+        </FormProvider>
 
-      <p>
-        قبلا ثبت نام کردید؟{" "}
-        <Link className="text-primary" href={"/login"}>
-          ورود
-        </Link>
-      </p>
+        <p>
+          قبلا ثبت نام کردید؟{" "}
+          <Link className="text-primary" href={"/login"}>
+            ورود
+          </Link>
+        </p>
+      </Suspense>
     </>
   );
 }
